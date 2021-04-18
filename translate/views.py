7d637b1a9670fd380d28564e71f294e1a397ab7c
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-17 23:31:32 -0300

    Divide add translate function for less complexity
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- translate/views.py
+++ translate/views.py
@@ -75,74 +75,80 @@ def delete_translate(request, id):
     else:
         return redirect('/')
 
+def add_translate_post(request, id):
+    phrase_formset = PhraseFormSet(prefix='phrase', data=request.POST)
+    word_portugueses_formset = WordPortugueseFormSet(prefix='word-portuguese', data=request.POST)
+    word_kokama_form = WordKokamaForm(data=request.POST)
+    pronunciation_choises_form = PronunciationChoisesForm(data=request.POST)
+    all_forms_are_valid = phrase_formset.is_valid() and word_portugueses_formset.is_valid() and word_kokama_form.is_valid() and pronunciation_choises_form.is_valid()
+    if (all_forms_are_valid):
+        url = URL.format(base_url = config('TRANSLATE_MICROSERVICE_URL'), parameter = "traducao/adicionar_traducao", id = id)
+        requests.post(url, data=request.POST)
+        return redirect('/traducao/lista_de_palavras')
+    else:
+        return render(request, 'add_word.html', {
+            'phrase_formset': phrase_formset,
+            'word_portugueses_formset': word_portugueses_formset,
+            'word_kokama_form': word_kokama_form,
+            'pronunciation_choises_form': pronunciation_choises_form
+        })
+
+def add_translate_get(request, id):
+    if id:
+        url = URL.format(base_url = config('TRANSLATE_MICROSERVICE_URL'), parameter = 'traducao/dicionario', id=id)
+        data = requests.get(url).json()
+        pronunciation_type = 0
+        if data['pronunciation_type'] == 'feminino':
+            pronunciation_type = 2
+        elif data['pronunciation_type'] == 'masculino':
+            pronunciation_type = 3
+        else:
+            pronunciation_type = 1
+
+        word_portuguese_data = {
+            'word-portuguese-TOTAL_FORMS': len(data['translations']),
+            'word-portuguese-INITIAL_FORMS': '0',
+            'word-portuguese-MIN_NUM_FORMS': '0',
+            'word-portuguese-MAX_NUM_FORMS': '1000',
+        }
+        for index, word in enumerate(data['translations']):
+            word_portuguese_data['word-portuguese-{}-word_portuguese'.format(index)] = word
+
+        phrase_data = {
+            'phrase-TOTAL_FORMS': len(data['phrases']),
+            'phrase-INITIAL_FORMS': '0',
+            'phrase-MIN_NUM_FORMS': '0',
+            'phrase-MAX_NUM_FORMS': '1000',
+        }
+        for index, phrase in enumerate(data['phrases']):
+            phrase_data['phrase-{}-phrase_portuguese'.format(index)] = phrase['phrase_portuguese']
+            phrase_data['phrase-{}-phrase_kokama'.format(index)] = phrase['phrase_kokama']
+
+        phrase_formset = PhraseFormSet(prefix='phrase', data=phrase_data)
+        word_portuguses_formset = WordPortugueseFormSet(prefix='word-portuguese', data=word_portuguese_data)
+        word_kokama_form = WordKokamaForm(data=data)
+        pronunciation_choises_form = PronunciationChoisesForm(data={'pronunciation_choises':pronunciation_type})
+    else:
+        phrase_formset = PhraseFormSet(prefix='phrase')
+        word_portuguses_formset = WordPortugueseFormSet(prefix='word-portuguese')
+        word_kokama_form = WordKokamaForm()
+        pronunciation_choises_form = PronunciationChoisesForm()
+    return render(request, 'add_word.html', {
+        'phrase_formset': phrase_formset,
+        'word_portuguses_formset': word_portuguses_formset,
+        'word_kokama_form': word_kokama_form,
+        'pronunciation_choises_form': pronunciation_choises_form,
+        'id': id
+    })
+
 @require_http_methods(["GET", "POST"])
 def add_translate(request, id):
     if request.user.is_superuser:
         try:
             if request.method == "GET":
-                if id:
-                    url = URL.format(base_url = config('TRANSLATE_MICROSERVICE_URL'), parameter = 'traducao/dicionario', id=id)
-                    data = requests.get(url).json()
-                    pronunciation_type = 0
-                    if data['pronunciation_type'] == 'feminino':
-                        pronunciation_type = 2
-                    elif data['pronunciation_type'] == 'masculino':
-                        pronunciation_type = 3
-                    else:
-                        pronunciation_type = 1
-
-                    word_portuguese_data = {
-                        'word-portuguese-TOTAL_FORMS': len(data['translations']),
-                        'word-portuguese-INITIAL_FORMS': '0',
-                        'word-portuguese-MIN_NUM_FORMS': '0',
-                        'word-portuguese-MAX_NUM_FORMS': '1000',
-                    }
-                    for index, word in enumerate(data['translations']):
-                        word_portuguese_data['word-portuguese-{}-word_portuguese'.format(index)] = word
-
-                    phrase_data = {
-                        'phrase-TOTAL_FORMS': len(data['phrases']),
-                        'phrase-INITIAL_FORMS': '0',
-                        'phrase-MIN_NUM_FORMS': '0',
-                        'phrase-MAX_NUM_FORMS': '1000',
-                    }
-                    for index, phrase in enumerate(data['phrases']):
-                        phrase_data['phrase-{}-phrase_portuguese'.format(index)] = phrase['phrase_portuguese']
-                        phrase_data['phrase-{}-phrase_kokama'.format(index)] = phrase['phrase_kokama']
-
-                    phrase_formset = PhraseFormSet(prefix='phrase', data=phrase_data)
-                    word_portuguses_formset = WordPortugueseFormSet(prefix='word-portuguese', data=word_portuguese_data)
-                    word_kokama_form = WordKokamaForm(data=data)
-                    pronunciation_choises_form = PronunciationChoisesForm(data={'pronunciation_choises':pronunciation_type})
-                else:
-                    phrase_formset = PhraseFormSet(prefix='phrase')
-                    word_portuguses_formset = WordPortugueseFormSet(prefix='word-portuguese')
-                    word_kokama_form = WordKokamaForm()
-                    pronunciation_choises_form = PronunciationChoisesForm()
-                return render(request, 'add_word.html', {
-                    'phrase_formset': phrase_formset,
-                    'word_portuguses_formset': word_portuguses_formset,
-                    'word_kokama_form': word_kokama_form,
-                    'pronunciation_choises_form': pronunciation_choises_form,
-                    'id': id
-                })
+                return add_translate_get(request, id)
             elif request.method == "POST":
-                phrase_formset = PhraseFormSet(prefix='phrase', data=request.POST)
-                word_portugueses_formset = WordPortugueseFormSet(prefix='word-portuguese', data=request.POST)
-                word_kokama_form = WordKokamaForm(data=request.POST)
-                pronunciation_choises_form = PronunciationChoisesForm(data=request.POST)
-                all_forms_are_valid = phrase_formset.is_valid() and word_portugueses_formset.is_valid() and word_kokama_form.is_valid() and pronunciation_choises_form.is_valid()
-                if (all_forms_are_valid):
-                    url = URL.format(base_url = config('TRANSLATE_MICROSERVICE_URL'), parameter = "traducao/adicionar_traducao", id = id)
-                    requests.post(url, data=request.POST)
-                    return redirect('/traducao/lista_de_palavras')
-                else:
-                    return render(request, 'add_word.html', {
-                        'phrase_formset': phrase_formset,
-                        'word_portugueses_formset': word_portugueses_formset,
-                        'word_kokama_form': word_kokama_form,
-                        'pronunciation_choises_form': pronunciation_choises_form
-                    })
+                return add_translate_post(request, id)
         except Exception:
             return HttpResponse(
                 SERVER_ERROR,
