4af1612eefbae314a2d4b8027ad6c8268cd25253
luisgaboardi <luis.ggaboardi@gmail.com>
2021-05-16 13:12:31 -0300

    Create custom email message for password redefinition

--- administration/views.py
+++ administration/views.py
@@ -81,7 +81,7 @@ def password_reset_request(request):
 					email_template_name = "password/password_reset_email.txt"
 					c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'127.0.0.1:8002',
 					'site_name': 'Projeto Kokama',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
